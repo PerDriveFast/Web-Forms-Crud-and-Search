3299931684e38f9923087276d381300ca92ea287
Trung_test <nrong157214@gmail.com>
2023-12-23 22:13:45 +0700

    Add project files.

---
+++ Users.aspx
@@ -0,0 +1,144 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Users.aspx.cs" Inherits="WebApplication1.Users" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+    <style>
+        table {
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        th,
+        td {
+            text-align: left;
+            padding: 8px;
+        }
+
+        th {
+            background-color: #4CAF50;
+            color: white;
+        }
+
+        tr:nth-child(even) {
+            background-color: #f2f2f2;
+        }
+
+        tr.separator {
+            border-top: 1px solid #ddd;
+            border-bottom: 1px solid;
+        }
+    </style>
+
+    <div class="container">
+        <div class="card-header">
+      <br />
+   
+
+</div>
+
+        <div class="modal fade" id="mymodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-backdrop="false">
+            <div class="modal-dialog modal-dailog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h4 class="modal-title">Add User New</h4>
+                        <asp:Label ID="lblmsg" Text="" runat="server" />
+                        <button type="button" class="close" data-dismiss="modal" >&times;</button>
+                    </div>
+                    <div class="modal-body">
+                        <formview>
+                            <div class="form-group">
+                                <label>Mã nhân viên</label>
+                                <asp:TextBox ID="txtMaNV" CssClass="form-control" placeholder="Mã nhân viên" runat="server" />
+                            </div>
+                            <div class="form-group">
+                                <label>Tên nhân viên</label>
+                                <asp:TextBox ID="txtTenNV" CssClass="form-control" placeholder="Tên nhân viên" runat="server" />
+                            </div>
+                            <div class="form-group">
+                                <label>Mật khẩu nhân viên</label>
+                                <asp:TextBox ID="txtMK" CssClass="form-control" placeholder="Mật khẩu nhân viên" runat="server" />
+                            </div>
+                            <div class="form-group">
+                                <label>Email nhân viên</label>
+                                <asp:TextBox ID="txtEmail" CssClass="form-control" placeholder="Email nhân viên" runat="server" />
+                            </div>
+                            <div class="form-group">
+                                <label>Số điện thoại nhân viên</label>
+                                <asp:TextBox ID="txtTel" CssClass="form-control" placeholder="Số điện thoại nhân viên" runat="server" />
+                            </div>
+                     <asp:HiddenField  ID="hdid"  runat="server"/>
+
+                        </formview>
+                    </div>
+                    <div class="modal-footer">
+                        <asp:Button style="float: left" ID="btnCreate" Text="Save" class="btn btn-primary" OnClick="btnCreate_Click" runat="server" />
+                        <asp:Button style="float: left" ID="btnClose" Text="Close" class="btn btn-danger"  OnClick="btnClose_Click" runat="server" />
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+ 
+    <section Id="section">
+        <div class="row match-height">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-header">
+                        <asp:Button Text="Add User" ID="modal" CssClass="btn btn-primary" OnClick="modal_Click" runat="server" />
+                       <asp:TextBox ID="txtSearch" runat="server" placeholder="Enter keyword"></asp:TextBox>
+                        <asp:Button ID="btnSearch" runat="server" Text="Search" OnClick="btnSearch_Click" />
+
+                    </div>
+                    <br />
+
+                    <div class="card-content">
+                        <div class="card-body">
+                            <div class="col-md-12 col-12">
+                                <table class="table">
+                                    <asp:Repeater ID="rptr1" DataSourceID="ds1" runat="server" OnItemDataBound="rptr1_ItemDataBound">
+                                        <HeaderTemplate>
+                                            <tr>
+                                                <th>Mã nhân viên</th>
+                                                <th>Tên nhân viên</th>
+                                                <th>Email nhân viên</th>
+                                                <th>Mật khẩu nhân viên</th>
+                                                <th>Số điện thoại nhân viên</th>
+                                                <th>Hành động</th>
+                                            </tr>
+                                        </HeaderTemplate>
+                                        <ItemTemplate>
+                                            <tr class="separator">
+                                                <td><%# Eval("UserID") %></td>
+                                                <td><%# Eval("UserName") %></td>
+                                                <td><%# Eval("Email") %></td>
+                                                <td ><%# Eval("Passwords") %></td>
+                                                <td><%# Eval("Tel") %></td>
+                                                <td>
+                                                    <asp:LinkButton ID="btnupdate" CommandName="Update" OnCommand="btnupdate_Command" CommandArgument='<%#Eval("UserID") %>' CssClass="btn btn-sm brn-primary"  runat="server" ><i  class="glyphicon glyphicon-pencil"></i></asp:LinkButton>
+
+                                                    <asp:LinkButton CommandName="Delete" ID="btndlt" CommandArgument='<%#Eval("UserID") %>' 
+                                                            OnClientClick="return confirm('Bạn có muốn xóa không !');"
+                                                            OnCommand="btndlt_Command" CssClass="btn btn-sm brn-danger" runat="server" ><i  class="glyphicon glyphicon-trash"></i></asp:LinkButton>
+                                                </td>
+                                            </tr>
+                                        </ItemTemplate>
+                                 
+                                    </asp:Repeater>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <asp:SqlDataSource ID="ds1" ConnectionString="<%$ConnectionStrings:connection %>" runat="server" SelectCommand="Select * from Users" />
+
+    <asp:Label ID="lblNoData" runat="server" Visible="false">Không có dữ liệu nhân viên</asp:Label>
+
+
+
+</asp:Content>
+
---
+++ Users.aspx.cs
@@ -0,0 +1,225 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace WebApplication1
+{
+    public partial class Users : System.Web.UI.Page
+    {
+        public void NewData()
+        {
+            txtMaNV.Text = "";
+            txtTenNV.Text = "";
+            txtEmail.Text = "";
+            txtMK.Text = "";
+            txtTel.Text = "";
+            lblmsg.Text = "";
+            // Disable the textbox after insertion
+            txtMaNV.Enabled = true;
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void modal_Click(object sender, EventArgs e)
+        {
+            string script = "$('#mymodal').modal('show');";
+            ClientScript.RegisterStartupScript(this.GetType(), "Popup", script, true);
+        }
+
+        protected void btnCreate_Click(object sender, EventArgs e)
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Command
+                SqlCommand cmd;
+
+                if (!string.IsNullOrEmpty(hdid.Value))
+                {
+                    string id = hdid.Value;
+                    cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "pcd_UpadteUser";
+                    cmd.Parameters.AddWithValue("@userid", id);
+                }
+                else
+                {
+                    cmd = new SqlCommand();
+                    cmd.Connection = conn;
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.CommandText = "pcd_SaveUsers";
+                }
+
+                // Add parameters with correct names
+                cmd.Parameters.AddWithValue("@username", txtTenNV.Text);
+                cmd.Parameters.AddWithValue("@email", txtEmail.Text);
+                cmd.Parameters.AddWithValue("@password", txtMK.Text);
+                cmd.Parameters.AddWithValue("@tel", txtTel.Text);
+
+                // For the case where it's not an update, add @userid parameter
+                if (string.IsNullOrEmpty(hdid.Value))
+                {
+                    cmd.Parameters.AddWithValue("@userid", txtMaNV.Text);
+                }
+
+                int rowsAffected = cmd.ExecuteNonQuery();
+                conn.Close();
+
+                if (rowsAffected > 0)
+                {
+                    lblmsg.Text = "Data Inserted Successfully";
+                }
+                else
+                {
+                    lblmsg.Text = "Error While Inserting Data";
+                }
+
+                string script = @"$('#mymodal').modal('hide');";
+                ClientScript.RegisterStartupScript(this.GetType(), "HideModal", script, true);
+
+                rptr1.DataBind();
+                NewData();
+            }
+        }
+
+
+
+        protected void btnupdate_Command(object sender, CommandEventArgs e)
+        {
+            string id = e.CommandArgument.ToString();
+            hdid.Value = id;
+
+            string connectionstring = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("GetUserData", conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                cmd.Parameters.AddWithValue("@userid", id);
+
+                SqlDataReader dataReader = cmd.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+                    txtMaNV.Text = dataReader["UserID"].ToString();
+                    txtMaNV.Enabled = false;
+                    txtTenNV.Text = dataReader["UserName"].ToString();
+                    txtEmail.Text = dataReader["Passwords"].ToString();
+                    txtMK.Text = dataReader["Email"].ToString();
+                    txtTel.Text = dataReader["Tel"].ToString();
+                }
+
+                dataReader.Close();
+            }
+
+
+            ScriptManager.RegisterStartupScript(this, GetType(), "OpenModalScript", "$('#mymodal').modal('show');", true);
+        }
+
+        protected void btnClose_Click(object sender, EventArgs e)
+        {
+            NewData();
+        }
+
+        protected void btndlt_Command(object sender, CommandEventArgs e)
+        {
+            string id = e.CommandArgument.ToString();
+
+            string connectionstring = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "pcd_DeleteUsers";
+                // Correct the parameter name to match the stored procedure
+                cmd.Parameters.AddWithValue("userID", id);
+                cmd.Connection = conn;
+                cmd.ExecuteNonQuery();
+            }
+
+            rptr1.DataBind();
+           
+        }
+
+
+
+        protected void btnSearch_Click(object sender, EventArgs e)
+        {
+            string keyword = txtSearch.Text.Trim();
+
+            string connectionstring = ConfigurationManager.ConnectionStrings["connection"].ConnectionString;
+
+            using (SqlConnection conn = new SqlConnection(connectionstring))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand();
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.CommandText = "SearchUsers"; // Replace with your stored procedure name
+                cmd.Parameters.AddWithValue("@SearchKeyword", keyword);
+                cmd.Connection = conn;
+
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                adapter.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    rptr1.DataSourceID = null; // Remove any direct assignment of DataSource
+                    rptr1.DataSource = dt;
+          
+                    rptr1.DataBind();
+                    rptr1.Visible = true; // Ensure Repeater is visible
+                    lblNoData.Visible = false; // Hide the label when there is data
+                }
+                else
+                {
+                    // No data, handle it as needed
+                    rptr1.Visible = false; // Optionally, hide the Repeater
+                    lblNoData.Visible = true; // Show a label with a message
+                }
+
+            }
+        }
+
+        protected void rptr1_ItemDataBound(object sender, RepeaterItemEventArgs e)
+        {
+            if (e.Item.ItemType == ListItemType.Footer && rptr1.Items.Count == 0)
+            {
+                Label lblNoData = (Label)e.Item.FindControl("lblNoData");
+                if (lblNoData != null)
+                {
+                    lblNoData.Visible = true;
+                }
+            }
+        }
+
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
